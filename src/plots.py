b2663821f0d6b549b4047bbe915e6e968113f168
michaelanofski <michaelanofski@gmail.com>
2026-01-28 19:25:07 +0100

    Update plots.py

--- src/plots.py
+++ src/plots.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from src.config import FIG_SIZE, DPI, COLOR_WOMEN, COLOR_MEN, OUTPUT_DIR
 
-def plot_women_vs_men_trend(df):
+def plot_women_vs_men(df):
     plt.figure(figsize=FIG_SIZE)
     plt.plot(df["Year"], df["Women"], label="Women", color=COLOR_WOMEN)
     plt.plot(df["Year"], df["Men"], label="Men", color=COLOR_MEN)
@@ -13,11 +13,11 @@ def plot_women_vs_men_trend(df):
     plt.figtext(0.5, -0.15, "Source: INKAR (BBSR)", ha="center", fontsize=9)
 
     plt.tight_layout()
-    plt.savefig(OUTPUT_DIR / "01_women_vs_men_trend.png", dpi=DPI)
+    plt.savefig(OUTPUT_DIR / "01_women_vs_men.png", dpi=DPI)
     plt.close()
 
 
-def plot_gender_gap_trend(df):
+def plot_gender_gap(df):
     plt.figure(figsize=FIG_SIZE)
     plt.plot(df["Year"], df["GenderGap"], color="black")
 
@@ -28,11 +28,11 @@ def plot_gender_gap_trend(df):
     plt.figtext(0.5, -0.15, "Source: INKAR (BBSR)", ha="center", fontsize=9)
 
     plt.tight_layout()
-    plt.savefig(OUTPUT_DIR / "02_gender_gap_trend.png", dpi=DPI)
+    plt.savefig(OUTPUT_DIR / "02_gender_gap_", dpi=DPI)
     plt.close()
 
 
-def plot_overall_unemployment_trend(df):
+def plot_overall_trend(df):
     plt.figure(figsize=FIG_SIZE)
     plt.plot(df["Year"], df["Total"], color="gray")
 
@@ -42,5 +42,5 @@ def plot_overall_unemployment_trend(df):
     plt.figtext(0.5, -0.15, "Source: INKAR (BBSR)", ha="center", fontsize=9)
 
     plt.tight_layout()
-    plt.savefig(OUTPUT_DIR / "03_overall_unemployment_trend.png", dpi=DPI)
+    plt.savefig(OUTPUT_DIR / "03_overall_trend.png", dpi=DPI)
     plt.close()
